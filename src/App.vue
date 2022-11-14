c3c716ec137fc5932f8a07e38ccbffd4f88e6311
Oghenekohworo Oghenefego <ofegofavour@gmail.com>
2022-11-14 21:56:59 +0100

    ftn

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,25 @@ const input_content = ref('')
 const input_category = ref(null)
 
 const todo_asc = computed(() => todos.value.sort((a, b) => {
-    return b.createdAt - a.createdAt
+    return a.createdAt - b.createdAt
 }))
 
 const addTodo = () => {
-
+    if (input_content.value.trim() === '' || input_category.value === null){
+       return
+    }
+    
+    todos.value.push({
+       content: input_content.value,
+       category: input_category.value,
+       done: false,
+       createdAt: new Date().getTime()
+    })
 }
+
+watch(todos, newVal => {
+   localStorage.setItem('todos', JSON.stringify(newVal))
+}, {deep: true})
 
 watch(name, (newVal) => {
    localStorage.setItem('name', newVal)
@@ -21,6 +34,7 @@ watch(name, (newVal) => {
 
 onMounted(() => {
    name.value = localStorage.getItem('name') || ''
+   todos.value = JSON.parse(localStorage.getItem('todos') || [])
 })
 </script>
 
@@ -47,12 +61,35 @@ onMounted(() => {
                                       <input 
                                           type="radio" 
                                           name="category" 
-                                          id="category1"
                                            value="business"
                                            v-model="input_category"/>
+                                           <span class="bubble business"></span>
+                                           <div>Business</div>
+                                  </label>
+                                  <label>
+                                      <input 
+                                          type="radio" 
+                                          name="category" 
+                                           value="personal"
+                                           v-model="input_category"/>
+                                           <span class="bubble personal"></span>
+                                           <div>Personal</div>
                                   </label>
                            </div>
+                           <input type="submit" value="Add todo" />
                  </form>
+           </section>
+           <section>
+                <h3>TODO LIST</h3>
+                <div class="list">
+                    <div v-for="todo in todo_asc" :class="`todo-item ${todo.done && 'done'}`">
+                        
+                      <label>
+                          <input type="checkbox"  v-model="todo.done" />
+                          <span></span>
+                      </label>
+                    </div>
+                </div>
            </section>
       </main>
 </template>
